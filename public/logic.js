174b7a6c08535c1b8bf43e47b197af93a181b8ec
Shahin-Sayadi <Shahin_sayadi@hotmail.com>
2021-09-09 15:38:27 +0200

    gif fix

--- public/logic.js
+++ public/logic.js
@@ -10,10 +10,10 @@ const input = document.getElementById("input");
 let userName;
 
 //forces user to choose name when app starts, and emits it to server
-window.onload = () => {
+/* window.onload = () => {
   userName = prompt("Enter your name?");
   socket.emit("user connected", userName);
-};
+}; */
 
 //emits input when submit-button is triggered, unless input is empty
 //if input starts with "/" -->
@@ -21,9 +21,11 @@ form.addEventListener("submit",function (e) {
   e.preventDefault();
   msg = input.value;
   
+  socket.emit("chat message", { userName, msg });
+    
   if (msg) {
     if (msg.startsWith("/")) {
-      collectText(msg)
+      collectText()
     } else {
       socket.emit("chat message", { userName, msg });
       var item = document.createElement("li");
@@ -41,15 +43,32 @@ socket.on("chat message", function (msg) {
   const typing = document.getElementById("typing");
   messages.removeChild(typing);
 
-  const item = document.createElement("li");
-  item.textContent = msg.userName + ": " + msg.msg;
+  if(msg.url){
+    const item = document.createElement("li");
+ const img = document.createElement("img")
+ img.src = msg.url
   messages.appendChild(item);
+  iTextMetrics.appendChild(img)
   window.scrollTo(0, document.body.scrollHeight);
+  } else {
+
+    const item = document.createElement("li");
+    item.textContent = msg.userName + ": " + msg.msg;
+    messages.appendChild(item);
+    window.scrollTo(0, document.body.scrollHeight);
+  }
 });
 
 //if user types, emit to server
 input.addEventListener("input", function (e) {
   e.preventDefault();
+  msg = input.value;
+  if (msg.startsWith("/")) {
+      collectText(msg)
+  } else {
+    gifDiv = document.getElementById("gifDiv")
+    messages.removeChild(gifDiv)
+  }
   msg = userName + " is typing...";
   socket.emit("typing", { msg });
 });
@@ -84,52 +103,77 @@ socket.on("user disconnected", function (msg) {
   window.scrollTo(0, document.body.scrollHeight);
 });
 
-async function collectText(){
-  const textToDisplay = await makeRequest("http://localhost:3000/api", "GET")
+
+
+async function collectText(msg){
+  const textToDisplay = await makeRequest("https://api.giphy.com/v1/gifs/trending?api_key=hXZ9UKHaXXv9rxb3kMfISfbwuyu4ydTJ&limit=25&rating=g", "GET")
   
+  const item = document.createElement("div");
+  item.id = "gifDiv";
+  messages.appendChild(item);
+  
+  if(msg == "/"){
+
+    const text = document.createElement("p")
+    text.innerHTML = "/trending"
+    
+    
+    
+    item.appendChild(text)
+    
+    
+
+
+  } else if (msg == "/trending"){
+    textToDisplay.data.map(e=>{
+  
+      
+        const imgContainer = document.createElement("img")
+        imgContainer.style.height = "50px"
+        imgContainer.style.width = "50px"
+        url = e.images.downsized.url
+        imgContainer.src = url
+        /* imgContainer.onclick(() => {
+          socket.emit("chat message", { url: e.images.downsized.url })
+        }) */
+        imgContainer.addEventListener("click", ()=>{socket.emit("chat message", { url: url })})
+        item.appendChild(imgContainer)
+        window.scrollTo(0, document.body.scrollHeight);
+      })
+
+  }
  
-  if(msg =="/"){
-    
-  
-    for (let index = 0; index < textToDisplay.length; index++) {
-      const commandList = textToDisplay[index];
-    
-    const item = document.createElement("li");
-    item.id = "typing";
-    item.innerHTML = "Aviable Commands:" + " " + "/" + commandList.commands
-    messages.appendChild(item);
-    window.scrollTo(0, document.body.scrollHeight);
-  }
-    console.log(textToDisplay)
 
-  } else if (msg == "/dog"){
+
+  /* for (let index = 0; index < textToDisplay.length; index++) {
+    const commandList = textToDisplay[index];
+    console.log(commandList)
+
+
+    const text = document.createElement("p")
+    text.innerText = "/gif"
+    const imgContainer = document.createElement("img")
+    imgContainer.style.height = "100px"
+    imgContainer.style.width = "100px"
+    imgContainer.src = textToDisplay.data[index].images.downsized.url
     
-  
-    const item = document.createElement("img");
-    item.id = "typing";
-    item.src = textToDisplay[0].image
-    messages.appendChild(item);
+    item.appendChild(text)
+    item.appendChild(imgContainer)
     window.scrollTo(0, document.body.scrollHeight);
   
-    console.log(textToDisplay[0].image)
-
-  } else if (msg == "/cat"){
-    console.log(textToDisplay[1].image)
-  }
-
-
+  } */
 }
 
 
-async function makeRequest(url, method, headers, body){
+async function makeRequest(url, method, body){
   try {
     const response = await fetch(url, {
-      headers,
+      
       method,
       body: JSON.stringify(body),
     })
     
-    console.log(response)
+    
     const result = await response.json()
     
     
